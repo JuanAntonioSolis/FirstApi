f3b19fed01dce11f48b80706ca68c3cbfd6629af
JuanAntonioSolis <jsolcor457@g.educaand.es>
2026-01-30 17:05:55 +0100

    30JanApiDoneJWT

--- src/main/java/com/jaroso/firstapi/controllers/AuthController.java
+++ src/main/java/com/jaroso/firstapi/controllers/AuthController.java
@@ -2,9 +2,11 @@ package com.jaroso.firstapi.controllers;
 
 import com.jaroso.firstapi.dtos.AuthDto;
 import com.jaroso.firstapi.dtos.UserCreateDto;
+import com.jaroso.firstapi.dtos.UserDto;
 import com.jaroso.firstapi.dtos.UserLoginDto;
 import com.jaroso.firstapi.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,8 +21,15 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthDto> register(@RequestBody UserCreateDto user) {
-        return authService.save(user);
+    public ResponseEntity<UserDto> register(@RequestBody UserCreateDto user) {
+        ResponseEntity<UserDto> response;
+        try{
+            response = ResponseEntity.status(HttpStatus.CREATED).body(authService.save(user));
+        } catch(RuntimeException e){
+
+            return ResponseEntity.badRequest().body(null);
+        }
+        return response;
     }
 
     @PostMapping("/login")
--- src/main/java/com/jaroso/firstapi/services/AuthService.java
+++ src/main/java/com/jaroso/firstapi/services/AuthService.java
@@ -2,18 +2,33 @@ package com.jaroso.firstapi.services;
 
 import com.jaroso.firstapi.dtos.AuthDto;
 import com.jaroso.firstapi.dtos.UserCreateDto;
+import com.jaroso.firstapi.dtos.UserDto;
+import com.jaroso.firstapi.dtos.UserLoginDto;
 import com.jaroso.firstapi.entities.User;
 import com.jaroso.firstapi.repositories.UserRepository;
+import com.jaroso.firstapi.security.JwtService;
 import com.jaroso.firstapi.security.PasswordConfig;
 import com.jaroso.firstapi.security.UserAuthority;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AuthService {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private UserRepository userRepository;
@@ -21,7 +36,18 @@ public class AuthService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public AuthDto save(UserCreateDto userDTO) {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtService jwtService;
+
+
+    /**
+     * Registra un nuevo usuario en BBDD
+     * @param userDTO
+     * @return
+     */
+    public UserDto save(UserCreateDto userDTO) {
         User user = new User(
                 null,
                 userDTO.username(),
@@ -30,9 +56,50 @@ public class AuthService {
                 List.of(UserAuthority.READ)
         );
 
+        //Comprobar que el username no esté ya en la BBDD
+        if (userRepository.findByUsername(userDTO.username()).isPresent()) {
+            log.error("El usuario ya existe");
+            throw new RuntimeException("El usuario ya existe");
+        }
+
+        //Si no existe se inserta en BBDD y devolvemos UserDto
         this.userRepository.save(user);
 
-        return null;
+        return new UserDto(user.getId(), user.getUsername(), user.getEmail());
     }
+
+    /**
+     * Login de un usuario
+     * @param user
+     * @return AuthDto con el Token JWT
+     */
+    public ResponseEntity<AuthDto> login(UserLoginDto user) {
+
+        //1. Buscar usuario en bbdd
+        Optional<User> userOptional = this.userRepository.findByUsername(user.username());
+        if (userOptional.isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        //2. Comprobar que la contraseña concida con la del usuario
+
+        Authentication authDTO = new UsernamePasswordAuthenticationToken(user.username(), user.password());
+        Authentication authentication =
+                authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(user.username(), user.password())
+                );
+
+        //3. Generar token JWT
+        String token = jwtService.generateToken(authentication);
+
+        //4. Generar el objeto AuthDto con el token JWT y devolverlo en la respuesta http
+        User userEntity = (User) authentication.getPrincipal();
+        AuthDto auth = new AuthDto(userEntity.getUsername(),
+                userEntity.getAuthorities().stream().map(Object::toString).toList(),token);
+
+        return ResponseEntity.ok(auth);
+    }
+
+
 
 }
